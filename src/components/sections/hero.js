5125f02bfb18707dd33890c62dcdc90f19db2a7c
daniellau415 <daniellau415@gmail.com>
2021-10-19 21:11:40 -0700

    Change hero js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,19 +59,11 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Daniel Lau.</h2>;
+  const three = <h3 className="big-heading">I build iOS apps.</h3>;
   const four = (
     <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
+      <p>Somehow I became an iOS Engineer.</p>
     </>
   );
   const five = (
